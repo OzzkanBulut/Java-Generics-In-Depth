f449dffa1c356d0c0e91909da482f34381938c8d
OzzkanBulut <67637654+OzzkanBulut@users.noreply.github.com>
2022-11-12 16:18:58 +0300

    Update Cage.java

--- 2- Generics Internals/3- Create Your Own Generic/Cage.java
+++ 2- Generics Internals/3- Create Your Own Generic/Cage.java
@@ -3,19 +3,19 @@ public class Cage<E> { // We mage Cage Class Generic !
   private E animal1;
   private E animal2;
   
-  private E getanimal1(){
+  private E getAnimal1(){
     return animal1;
   }
   
-  private E getanimal2(){
+  private E getAnimal2(){
     return animal2;
   }
   
-  private E setanimal1(E animal1){
+  private E setAnimal1(E animal1){
     this.animal1 = animal1;
   }
   
-  private E setanimal1(E animal2){
+  private E setAnimal1(E animal2){
     this.animal2 = animal2;
   }
   
