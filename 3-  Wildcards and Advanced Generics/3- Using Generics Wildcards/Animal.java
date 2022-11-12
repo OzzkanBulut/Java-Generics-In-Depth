8f9184801ecf1463c5d9f27f232ea61fee2cf8e3
OzzkanBulut <67637654+OzzkanBulut@users.noreply.github.com>
2022-11-12 17:27:24 +0300

    Update Animal.java

--- 3-  Wildcards and Advanced Generics/3- Using Generics Wildcards/Animal.java
+++ 3-  Wildcards and Advanced Generics/3- Using Generics Wildcards/Animal.java
@@ -8,5 +8,19 @@ public class Animal {
   public String getType(){
     return type;
   }
+ 
+ // Overriding these two methods will make animal objects comparable
+ @Override
+    public boolean equals(Object o) { 
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(type, animal.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
   
 }
