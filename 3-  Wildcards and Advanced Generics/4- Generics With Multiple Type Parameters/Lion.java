887069c362010e48baddf133aee0d42aeb82cca6
OzzkanBulut <67637654+OzzkanBulut@users.noreply.github.com>
2022-11-12 20:28:39 +0300

    Update Lion.java

--- 3-  Wildcards and Advanced Generics/4- Generics With Multiple Type Parameters/Lion.java
+++ 3-  Wildcards and Advanced Generics/4- Generics With Multiple Type Parameters/Lion.java
@@ -1,3 +1,13 @@
-public class Lion extends Animal implements Runs {
-  
+
+public class Lion extends Animal implements Eats,Runs{
+    @Override
+    public void eat() {
+        System.out.println("Lion eats");
+    }
+
+    @Override
+    public void runs() {
+
+        System.out.println("Lion runs");
+    }
 }
