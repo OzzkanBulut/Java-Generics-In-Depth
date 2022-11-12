699a26096c652eaaf6695c50c21ad1494283f2aa
OzzkanBulut <67637654+OzzkanBulut@users.noreply.github.com>
2022-11-12 16:16:00 +0300

    Create Lion.java

---
+++ 2- Generics Internals/3- Create Your Own Generic/Lion.java
@@ -0,0 +1,22 @@
+public class Cage<E> { // We mage Cage Class Generic !
+  
+  private E animal1;
+  private E animal2;
+  
+  private E getanimal1(){
+    return animal1;
+  }
+  
+  private E getanimal2(){
+    return animal2;
+  }
+  
+  private E setanimal1(E animal1){
+    this.animal1 = animal1;
+  }
+  
+  private E setanimal1(E animal2){
+    this.animal2 = animal2;
+  }
+  
+  
